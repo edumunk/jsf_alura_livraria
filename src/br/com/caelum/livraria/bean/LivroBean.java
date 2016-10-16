7a8cfefce59350d47b689341045cd646356b374c
Eduardo Lacerda <lacerda.snr@gmail.com>
2016-10-16 17:00:30 -0200

    Exercises chapter 6

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -2,9 +2,12 @@ package br.com.caelum.livraria.bean;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.modelo.Livro;
@@ -34,7 +37,7 @@ public class LivroBean {
 
 	public List<Livro> getLivros() {
 		List<Livro> listaTodos = new DAO<Livro>(Livro.class).listaTodos();
-		
+
 		return listaTodos;
 	}
 
@@ -49,13 +52,23 @@ public class LivroBean {
 	public void gravar() {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
-		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+		if (livro.getAutores().isEmpty()) {			
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("Livro deve ter pelo menos um Autor."));
+			return;
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
-		
+
 		this.livro = new Livro();
+	}
+
+	public void comecaComDigitoUm(FacesContext fc, UIComponent componente, Object value) throws ValidatorException {
+		
+		String valor = value.toString();
+		
+		if(!valor.startsWith("1"))
+			throw new ValidatorException(new FacesMessage("O ISBN deve começar com digito 1"));
+
 	}
 
 }
